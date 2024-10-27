7b433966e6c89b3043fcf468c90c28715385def6
Nafis Abdullah Ahmad <n.ahmad.web.cit22@gmail.com>
2024-10-27 16:03:47 +0600

    post image wip

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,27 +3,7 @@
 @section('content')
     <main class="container max-w-xl mx-auto space-y-8 mt-8 px-2 md:px-0 min-h-screen">
         @if (Auth::user())
-            <!-- Barta Create Post Card -->
-            <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data"
-                class="bg-white border-2 border-black rounded-lg shadow mx-auto max-w-none px-4 py-5 sm:px-6 space-y-3">
-                @csrf
-                <div class="flex items-start">
-                    <div class="text-gray-700 font-normal w-full">
-                        <textarea
-                            class="block w-full p-2 pt-2 text-gray-900 rounded-lg border-none outline-none focus:ring-0 focus:ring-offset-0"
-                            name="content" rows="2" placeholder="What's going on, {{ Auth::user()->first_name }}?"></textarea>
-                    </div>
-                </div>
-                @error('content')
-                    <div class="text-sm text-red-600">{{ $message }}</div>
-                @enderror
-                <div class="flex items-center justify-end">
-                    <button type="submit"
-                        class="text-xs rounded-full px-4 py-2 font-semibold bg-gray-800 hover:bg-black text-white">
-                        Post
-                    </button>
-                </div>
-            </form>
+            @include('partials.create-post-card')
         @else
             <p class="text-center text-md text-gray-700">You must <a class="text-indigo-600"
                     href="{{ route('login') }}">login</a> to create a post.</p>
@@ -34,9 +14,12 @@
             @if ($search)
                 <p class="text-center text-md text-gray-700 flex items-center justify-center">
                     Showing search results for <strong class="ml-1">{{ $search }}</strong>
-                    <a href="{{ route('home') }}" class="ml-1 inline text-white text-xs items-center rounded-full p-1 bg-gray-800 hover:bg-black">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    <a href="{{ route('home') }}"
+                        class="ml-1 inline text-white text-xs items-center rounded-full p-1 bg-gray-800 hover:bg-black">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M6 18L18 6M6 6l12 12" />
                         </svg>
                     </a>
                 </p>
@@ -82,6 +65,31 @@
 
 @push('custom-scripts')
     <script>
+        const pictureInput = document.getElementById('picture');
+        const imagePreviewContainer = document.getElementById('image-preview-container');
+        const imagePreview = document.getElementById('image-preview');
+        const removeImageButton = document.getElementById('remove-image');
+
+        // Show image preview on file select
+        pictureInput.addEventListener('change', function() {
+            const file = this.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    imagePreview.src = e.target.result;
+                    imagePreviewContainer.style.display = 'relative';
+                }
+                reader.readAsDataURL(file);
+            }
+        });
+
+        // Remove image preview
+        removeImageButton.addEventListener('click', function() {
+            pictureInput.value = ''; // Clear the file input
+            imagePreview.src = ''; // Clear the image preview
+            imagePreviewContainer.style.display = 'none'; // Hide the preview container
+        });
+
         document.getElementById('load-more').addEventListener('click', function() {
             const button = this;
             const nextPageUrl = button.getAttribute('data-next-page');
--- resources/views/pages/posts/create.blade.php
+++ resources/views/pages/posts/create.blade.php
@@ -5,47 +5,8 @@
         <h3 class="text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Create New Post
         </h3>
-        <!-- Barta create Post Card -->
-        <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data"
-            class="bg-white border-2 border-black rounded-lg shadow mx-auto max-w-none px-4 py-5 sm:px-6 space-y-3">
-            @csrf
-            <!-- create Post Card Top -->
-            <div>
-                <div class="flex items-start /space-x-3/">
-                    <!-- Content -->
-                    <div class="text-gray-700 font-normal w-full">
-                        <textarea
-                            class="block w-full p-2 pt-2 text-gray-900 rounded-lg border-none outline-none focus:ring-0 focus:ring-offset-0"
-                            name="content" rows="2" placeholder="What's going on, {{ Auth::user()->first_name }}?"></textarea>
-                    </div>
-                </div>
-            </div>
-            @error('content')
-                <div class="text-sm text-red-600">
-                    {{ $message }}
-                </div>
-            @enderror
 
-            <!-- create Post Card Bottom -->
-
-            <div>
-                <!-- Card Bottom Action Buttons -->
-                <div class="flex items-center justify-end">
-
-                    <div>
-                        <!-- Post Button -->
-                        <button type="submit"
-                            class="-m-2 flex gap-2 text-xs items-center rounded-full px-4 py-2 font-semibold bg-gray-800 hover:bg-black text-white">
-                            Post
-                        </button>
-                        <!-- /Post Button -->
-                    </div>
-                </div>
-                <!-- /Card Bottom Action Buttons -->
-            </div>
-            <!-- /create Post Card Bottom -->
-        </form>
-        <!-- /Barta create Post Card -->
+        @include('partials.create-post-card')
 
         <!-- Home Button -->
         <div class="flex justify-center mt-6">
