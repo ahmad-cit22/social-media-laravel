c2080396261224604bdeffd076e4291f9b235612
Nafis Abdullah Ahmad <n.ahmad.web.cit22@gmail.com>
2024-10-27 17:34:47 +0600

    post image show

--- resources/views/partials/post-card.blade.php
+++ resources/views/partials/post-card.blade.php
@@ -5,13 +5,22 @@
         <div class="flex items-center justify-between">
             <div class="flex items-center space-x-3">
 
+                <!-- User Avatar -->
+                <div class="flex-shrink-0">
+                    <img class="h-10 w-10 rounded-full object-cover"
+                        src="{{ $post->author->avatar ? asset('storage/' . $post->author->avatar) : asset('storage/avatars/def-avatar.jpg') }}"
+                        alt="image-{{ $post->author->fullName }}" />
+                </div>
+                <!-- /User Avatar -->
+
                 <!-- User Info -->
                 <div class="text-gray-900 flex flex-col min-w-0 flex-1">
                     <a href="{{ route('profile.show', $post->author_id) }}"
                         class="hover:underline font-semibold line-clamp-1">
                         {{ $post->author->fullName }}
                         @if (Auth::user() && Auth::id() === $post->author_id)
-                            <span class="ml-1 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
+                            <span
+                                class="ml-1 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
                                 Its you
                             </span>
                         @endif
@@ -74,6 +83,12 @@
 
     <!-- Content -->
     <div class="py-4 text-gray-700 font-normal">
+        @if ($post->picture)
+            <img src="{{ asset('storage/' . $post->picture) }}"
+                                            alt="image-{{ $post->picture }}"
+                class="min-h-auto w-full rounded-lg object-cover max-h-64 md:max-h-72" />
+        @endif
+
         <p>
             {{ $post->content }}
         </p>
@@ -85,6 +100,30 @@
         <span class="">•</span>
         <span>{{ $post->views_count }} views</span>
     </div>
+
+    <!-- Barta Card Bottom -->
+    {{-- <footer class="border-t border-gray-200 pt-2">
+        <!-- Card Bottom Action Buttons -->
+        <div class="flex items-center justify-between">
+            <div class="flex gap-8 text-gray-600">
+                <!-- Comment Button -->
+                <button type="button"
+                    class="-m-2 flex gap-2 text-xs items-center rounded-full p-2 text-gray-600 hover:text-gray-800">
+                    <span class="sr-only">Comment</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                        stroke="currentColor" class="w-5 h-5">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z" />
+                    </svg>
+
+                    <p>8</p>
+                </button>
+                <!-- /Comment Button -->
+            </div>
+
+        </div>
+        <!-- /Card Bottom Action Buttons -->
+    </footer> --}}
 
 </article>
 <!-- /Barta Card -->
